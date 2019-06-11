0ed94b709cba09b89e77ec027629b9f0ed62bfcc
Jaakko Kauranen <jaakko.kauranen@metropolia.fi>
2019-06-12 00:26:28 +0300

    Navigation started

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import PlayerPage from './containers/PlayerPage/PlayerPage';
+import Navigation from './components/Navigation/Navigation';
 
 class App extends Component {
 
   render() {
     //this.getAllPlayersFromDatabase();
     return(
+      <div>
+      <Navigation />
       <PlayerPage />
+      </div>
     );
   }
 }
