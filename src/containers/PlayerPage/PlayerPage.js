db70697b21ebad6546c91c46aafe4645b02e1d07
Jaakko Kauranen <jaakko.kauranen@metropolia.fi>
2019-06-11 23:34:31 +0300

    Players in card format

--- src/containers/PlayerPage/PlayerPage.js
+++ src/containers/PlayerPage/PlayerPage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Player from '../../components/Players/Player/Player';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 class PlayerPage extends Component{
     state = {
@@ -17,9 +19,18 @@ class PlayerPage extends Component{
     render(){
         
         const players = this.state.players.map(player => {
-            return <Player key={player.playerId} image={player.image} name={player.name}/>
-        });
-        return <div>{players}</div>
+            return <Player key={player.playerId} 
+                image={player.image} 
+                name={player.name} 
+                stats={player.stats}
+                />
+        }).reverse(); // reverse the final array because firebase returns players in ascending order
+
+        return (<Container>
+            <Row>
+            {players}
+            </Row>
+        </Container>);
     };
 }
 
