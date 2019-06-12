049ca3de3b68c96ba677b6696a6aedf2932b45a4
DESKTOP-F83FKH7\Jaakko <jaakko.kauranen@gmail.com>
2019-06-12 19:44:16 +0300

    merge 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
-import PlayersPage from './containers/PlayerPage/PlayersPage';
-=======
 import PlayerPage from './containers/PlayerPage/PlayerPage';
 import Navigation from './components/Navigation/Navigation';
->>>>>>> 0ed94b709cba09b89e77ec027629b9f0ed62bfcc
 
 class App extends Component {
 
   render() {
     return(
-<<<<<<< HEAD
-      <PlayersPage />
-=======
       <div>
       <Navigation />
       <PlayerPage />
       </div>
->>>>>>> 0ed94b709cba09b89e77ec027629b9f0ed62bfcc
     );
   }
 }
