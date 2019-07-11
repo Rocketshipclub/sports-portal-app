95fb253e23feb749dd1eeb6ca0f822a58b50b410
Jaakko Kauranen <jaakko.kauranen@metropolia.fi>
2019-07-11 21:52:04 +0300

    deleted no longer used component

--- src/components/Players/Players.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Player from './PlayerCard/PlayerCard';
-
-const players = (props) => props.players.map((player, index) => {
-    return <Player key={index} player={player}/>
-})
-
-export default players;
