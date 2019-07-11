922de448165df6f54c373acf46ae2559e1062b4c
Jaakko Kauranen <jaakko.kauranen@metropolia.fi>
2019-07-11 21:49:57 +0300

    added sorting to player page

--- src/components/Players/Players.js
+++ src/components/Players/Players.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Player from './Player/Player';
+import Player from './PlayerCard/PlayerCard';
 
 const players = (props) => props.players.map((player, index) => {
     return <Player key={index} player={player}/>
--- src/containers/PlayerPage/PlayersPage.js
+++ src/containers/PlayerPage/PlayersPage.js
@@ -6,12 +6,14 @@ import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 import CardGroup from 'react-bootstrap/CardGroup';
 import Column from 'react-bootstrap/Col';
-import { Link } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
 
 class PlayersPage extends Component{
     state = {
         players: [],
-        selectedPlayer: ''
+        selectedPlayer: '',
+        sortBy: 'kills',
+        ascending: false
       }
     
     componentDidMount() {
@@ -25,7 +27,55 @@ class PlayersPage extends Component{
         this.setState({selectedPlayer: id});
     }
 
+    sortingHandler = (param) => {
+        this.setState({sortBy:param})
+    }
+
+    handleChange = () => {
+        this.setState({ascending: !this.state.ascending});
+    }
+
+    sortByKills = () => {
+        this.state.ascending ? 
+            this.state.players.sort((playerA, playerB) => (playerA.stats.kills > playerB.stats.kills) ? 1 : -1) :
+            this.state.players.sort((playerA, playerB) => (playerA.stats.kills < playerB.stats.kills) ? 1 : -1)
+    }
+
+    sortByAssists = () => {
+        this.state.ascending ? 
+            this.state.players.sort((playerA, playerB) => (playerA.stats.assists > playerB.stats.assists) ? 1 : -1) :
+            this.state.players.sort((playerA, playerB) => (playerA.stats.assists < playerB.stats.assists) ? 1 : -1)
+    }
+
+    sortByDeaths = () => {
+        this.state.ascending ? 
+            this.state.players.sort((playerA, playerB) => (playerA.stats.deaths > playerB.stats.deaths) ? 1 : -1) :
+            this.state.players.sort((playerA, playerB) => (playerA.stats.deaths < playerB.stats.deaths) ? 1 : -1) 
+    }
+
+    sortByTeam = () => {
+        this.state.ascending ?
+            this.state.players.sort((playerA, playerB) => (playerA.team > playerB.team) ? 1 : -1) :
+            this.state.players.sort((playerA, playerB) => (playerA.team < playerB.team) ? 1 : -1)
+    }
+
+        
     render(){
+        switch(this.state.sortBy){
+            case 'kills':
+                this.sortByKills();
+                break;
+            case 'deaths':
+                this.sortByDeaths();
+                break;
+            case 'assists':
+                this.sortByAssists();
+                break;
+            case 'team':
+                this.sortByTeam();
+                break;
+        }
+
         const players = this.state.players.map(player => {
             return (
             <Column md={3}>
@@ -35,11 +85,18 @@ class PlayersPage extends Component{
                 stats={player.stats}
                 playerId={player.playerId}
                 click={() => this.playerSelectedHandler}/></Column>
-        )}).reverse(); // reverse the final array because firebase returns players in ascending order
-
+        )}); // reverse the final array because firebase returns players in ascending order
+        
         return (
         <Container>
-            Sort by <Button>Kills</Button> <Button>Deaths</Button> <Button>Assists</Button> <Button>Team</Button>
+            Sort by <Button onClick={() => this.sortingHandler('kills')}>Kills</Button> 
+            <Button onClick={() => this.sortingHandler('deaths')}>Deaths</Button> 
+            <Button onClick={() => this.sortingHandler('assists')}>Assists</Button> 
+            <Button onClick={() => this.sortingHandler('team')}>Team</Button>
+            <Form>
+                <Form.Check type="checkbox" label="Ascending" onChange={() => this.handleChange()}/> 
+            </Form>
+
             <Row style={{paddingTop:'16px'}}>
                 <CardGroup>
                 {players}
